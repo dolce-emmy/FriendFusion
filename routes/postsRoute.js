54edc460f12296cf5ee9cd3779da905242524f8d
onyekapeter1989 <okaforonyekapeter94@gmail.com>
2023-05-03 10:45:10 +0200

    save

---
+++ routes/postsRoute.js
@@ -0,0 +1,38 @@
+
+
+
+//getAllPOst//
+
+app.get ('/',);
+
+
+// for delete post//
+
+app.delete('/:Id',);
+
+
+//get all single post//
+
+app.post('/:id',);
+
+
+// for new post//
+
+app.post('/', );
+
+// update post//
+
+
+app.patch('/:id', );
+
+//like post //
+
+
+app.get('/:id',);
+
+
+
+
+
+
+
