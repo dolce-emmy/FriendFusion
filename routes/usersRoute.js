07de0baf5bbb6b96ac8662877e0eb878e8756216
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-04 11:59:16 +0200

    testting the userrouts endpoint

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,56 +1,52 @@
-import express from "express";
-import { deleteUser, getAllUsers, getSingleUser, loginUser, refreshPage, registerUser, removeFriend, updateUser, addFriend, getUserFriends, searchUsers } from "../controllers/usersController.js";
-
+import express from 'express';
+import {
+    deleteUser,
+    getAllUsers,
+    getSingleUser,
+    loginUser,
+    refreshPage,
+    registerUser,
+    removeFriend,
+    updateUser,
+    addFriend,
+    getUserFriends,
+    searchUsers,
+} from '../controllers/usersController.js';
 
 const router = express.Router();
 
-
 // users/register
-router.post("/register", registerUser);
-
-
+router.post('/register', registerUser);
 
 // users/login
-router.post("/login", loginUser);
-
+router.post('/login', loginUser);
 
 // getAllUsers
-router.get("/", getAllUsers);
-
+router.get('/', getAllUsers);
 
 // get request "users/:id/friends/:id"
 
-router.get("/getUserFriends/:id", getUserFriends);
-
+router.get('/getUserFriends/:id', getUserFriends);
 
 // post request add users/:id/friends
-router.post("/addFriend/:id", addFriend);
+router.post('/addFriend/:id', addFriend);
 
 //
-router.delete("/removeFriend/:id", removeFriend); 
+router.delete('/removeFriend/:id', removeFriend);
 
 // get SingleUser
-router.get("/:id", getSingleUser);
-
+router.get('/:id', getSingleUser);
 
 // update singleUser
-router.patch("/:id", updateUser);
-  
+router.patch('/:id', updateUser);
+
 // delete singleUser
-router.delete("/:id", deleteUser);
-  
-  
+router.delete('/:id', deleteUser);
 
 // get refreshPage
-router.get("/refreshPage", refreshPage);
-  
-// get searchUsers
-router.get("/searchUsers/:searchTerm", searchUsers);
+router.get('/refreshPage', refreshPage);
 
+// get searchUsers
+router.get('/searchUsers/:searchTerm', searchUsers);
 
 export default router;
-
-
-
-
-
