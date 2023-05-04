85a877a82f93b66c84c33f1961a784b4e75483bd
Eman Yassin <eman.yassin83@gmail.com>
2023-05-04 11:47:10 +0200

    created auth

---
+++ middlewares/auth.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+import UserCollection from '../models/UserCollection.js';
+
+export const auth = async (req, res, next) => {
+
+    try{
+
+        const token = req.headers.token;
+
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await UserCollection.findById(payload.id)
+
+        req.user = user;
+
+        next();
+
+    }catch (error) {
+
+        res.status(401).json({ success: false, message: 'Unauthorized' });
+        
+    }
+}
