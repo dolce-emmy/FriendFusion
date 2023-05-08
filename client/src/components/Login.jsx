c332dfa2d6712d3e7afc1c24ef58006e1b713be1
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-08 15:59:35 +0200

    Completing the Navbar

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useState } from 'react';
 // import { MyContext } from '../context/ContextContainer';
 function Login() {
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,26 @@
 import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
-
+    return (
+        <header>
+        <div className='title-logo'>
+        <Link className='title-logo' to='/'>
+        Friend Fusion
+        </Link>
+        </div>
+        <nav className='nav'> 
+        <NavLink className='navlink' to='register'>
+        Register
+        </NavLink>
+        <NavLink className='navlink' to='profile'>
+        Profile
+        </NavLink>
+        <NavLink className='navlink' to='login'>
+        Login
+        </NavLink>
+        </nav>
+        </header>
+        );
 };
 
 export default Navbar;
