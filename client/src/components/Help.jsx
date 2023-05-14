764d1ba54ae70db52e879c4a1f38f06bc2808bbb
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-14 13:24:24 +0200

    completing the Help form

--- client/src/components/Help.jsx
+++ client/src/components/Help.jsx
@@ -1,61 +1,91 @@
 import React from 'react';
+import Header from './Header.jsx';
+import { Link } from 'react-router-dom';
 
 function Help() {
-  return (
-    <div>
-      <h2>Help Center</h2>
-      <ul>
-        <li>How to create an account</li>
-        <li>How to customize your profile</li>
-        <li>How to post content</li>
-        <li>How to connect with other users</li>
-        <li>How to adjust privacy settings</li>
-        <li>How to report inappropriate content or behavior</li>
-        <li>How to contact customer support</li>
-        <li>Frequently Asked Questions (FAQs)</li>
-        <li>Tips and tricks</li>
-        <li>Terms of service and community guidelines</li>
-      </ul>
-      
-      <h3>How to create an account</h3>
-      <p>To create an account, visit our website and click the "Sign up" button. You'll need to provide your email address, choose a password, and complete any additional fields we require.</p>
-      
-      <h3>How to customize your profile</h3>
-      <p>To customize your profile, click on your profile picture in the top right corner of the screen. From there, you can upload a profile picture, add a bio, and choose a cover photo.</p>
-      
-      <h3>How to post content</h3>
-      <p>To post content, click on the "Post" button on the homepage. You can post text updates, photos, videos, and links.</p>
-      
-      <h3>How to connect with other users</h3>
-      <p>To connect with other users, you can follow them, send friend requests, and join groups or communities. You can search for other users by name or by using our search bar.</p>
-      
-      <h3>How to adjust privacy settings</h3>
-      <p>To adjust your privacy settings, click on the "Settings" button on your profile page. From there, you can control who can see your posts and profile information, and how to block or report other users.</p>
-      
-      <h3>How to report inappropriate content or behavior</h3>
-      <p>If you see inappropriate content or behavior on our app, you can report it by clicking the "Report" button on the post or message in question. You can also block or report users who are behaving inappropriately.</p>
-      
-      <h3>How to contact customer support</h3>
-      <p>If you have any issues or questions about our app, you can contact our customer support team by emailing support@friendfusion.com, or by filling out our contact form on our website.</p>
-      
-      <h3>Frequently Asked Questions (FAQs)</h3>
-      <ul>
-        <li>How do I delete my account?</li>
-        <li>How do I change my password?</li>
-        <li>How do I retrieve a forgotten username?</li>
-      </ul>
-      
-      <h3>Tips and tricks</h3>
-      <ul>
-        <li>Use hashtags to increase your post's visibility</li>
-        <li>Schedule your posts for optimal engagement times</li>
-        <li>Interact with other users to build your network</li>
-      </ul>
-      
-      <h3>Terms of service and community guidelines</h3>
-      <p>For more information on our terms of service and community guidelines, please visit our website.</p>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className='max-w-2xl mx-auto py-8 px-4'>
+                <h2 className='text-3xl font-bold mb-8'>Help Center</h2>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            How to create an account
+                        </h3>
+                        <p className='mb-8'>
+                            To create an account, visit our website and click
+                            the "Sign up" button. You'll need to provide your
+                            email address, choose a password, and complete any
+                            additional fields we require.
+                        </p>
+                    </div>
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            How to customize your profile
+                        </h3>
+                        <p className='mb-8'>
+                            To customize your profile, click on your profile
+                            picture in the top right corner of the screen. From
+                            there, you can upload a profile picture, add a bio,
+                            and choose a cover photo.
+                        </p>
+                    </div>
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            How to post content
+                        </h3>
+                        <p className='mb-8'>
+                            To post content, click on the "Post" button on the
+                            homepage. You can post text updates, photos, videos,
+                            and links.
+                        </p>
+                    </div>
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            How to connect with other users
+                        </h3>
+                        <p className='mb-8'>
+                            To connect with other users, you can follow them,
+                            send friend requests, and join groups or
+                            communities. You can search for other users by name
+                            or by using our search bar.
+                        </p>
+                    </div>
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            How to adjust privacy settings
+                        </h3>
+                        <p className='mb-8'>
+                            To adjust your privacy settings, click on the
+                            "Settings" button on your profile page. From there,
+                            you can control who can see your posts and profile
+                            information, and how to block or report other users.
+                        </p>
+                    </div>
+
+                    <div className='border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg'>
+                        <h3 className='text-xl font-bold mb-4'>
+                            Frequently Asked Questions (FAQs)
+                        </h3>
+                        <ul className='list-disc list-inside mb-8 ml-4'>
+                            <li>How do I delete my account?</li>
+                            <li>How do I change my password?</li>
+                            <li>How do I retrieve a forgotten username?</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='flex justify-center mt-8'>
+                    <Link
+                        to='/'
+                        className='bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md'
+                    >
+                        Go to Home
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Help;
