39f37ff8e2bc2cb2ff45a238d531098bc19dedc1
rbinduc <107483540+rbinduc@users.noreply.github.com>
2023-05-16 18:35:15 +0200

    fix: public profile page and refactor header (#90)

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,68 +1,32 @@
-import React from 'react';
-import { useState } from 'react';
-import UserInfo from './UserInfo';
-import Header from './Header';
-import PostForm from './PostForm';
-import PostList from './PostList';
-import api from '../api';
+import React from "react";
+import UserInfo from "./UserInfo";
+import Header from "./Header";
+import PostForm from "./PostForm";
+import PostList from "./PostList";
+import { useAppContext } from "../context/AppContext";
 
 const Home = () => {
-    const [showSearchedUsers, setShowSearchedUsers] = useState(false);
-    const [searchedUsers, setSearchedUsers] = useState([]);
-
-    const handleSearch = (event) => {
-        event.preventDefault();
-        // Handle search functionality here
-        const query = event.target.search.value;
-
-        const formData = new FormData();
-        formData.append('query', query);
-
-        // Filter the user list based on the search query
-        api.post('/users/search', formData).then((res) => {
-            console.log(res.data.data);
-            setSearchedUsers(res.data.data);
-            setShowSearchedUsers(true);
-        });
-    };
-
-    const [isNightMode, setIsNightMode] = useState(false);
-
-    const handleToggleNightMode = () => {
-        // Handle night mode toggle functionality here
-        setIsNightMode(!isNightMode);
-        const body = document.querySelector('body');
-        body.classList.toggle('night-mode');
-    };
-
-    //const isNightMode = false;
-
-    return (
-        <div>
-            {/* Header */}
-            <Header
-                showSearchedUsers={showSearchedUsers}
-                searchedUsers={searchedUsers}
-                onSearch={handleSearch}
-                isNightMode={isNightMode}
-                onToggleNightMode={handleToggleNightMode}
-            />
-            <div className='w-5/6 mx-auto flex flex-col md:flex-row items-start gap-6 mt-10'>
-                <div className='w-full md:w-4/12 lg:w-3/12 lg:max-w-xs'>
-                    <UserInfo />
-                </div>
-
-                <div className='w-full md:w-8/12 lg:w-6/12 lg:max-w-xl'>
-                    <PostForm />
-                    <PostList />
-                </div>
-                <div className='w-full md:hidden lg:block lg:w-3/12 lg:max-w-xs'>
-                    {/* TODO: advertisements, friends list */}
-                </div>
-            </div>
-            {/* Footer */}
+  const { user, posts } = useAppContext();
+  return (
+    <div>
+      {/* Header */}
+      <Header />
+      <div className="w-5/6 mx-auto flex flex-col md:flex-row items-start gap-6 mt-10">
+        <div className="w-full md:w-4/12 lg:w-3/12 lg:max-w-xs">
+          <UserInfo user={user} />
         </div>
-    );
+
+        <div className="w-full md:w-8/12 lg:w-6/12 lg:max-w-xl">
+          <PostForm />
+          <PostList posts={posts} />
+        </div>
+        <div className="w-full md:hidden lg:block lg:w-3/12 lg:max-w-xs">
+          {/* TODO: advertisements, friends list */}
+        </div>
+      </div>
+      {/* Footer */}
+    </div>
+  );
 };
 
 export default Home;
--- client/src/components/PublicProfile.jsx
+++ client/src/components/PublicProfile.jsx
@@ -1,94 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../api';
+import PostList from "./PostList";
+import UserInfo from "./UserInfo";
+import Header from "./Header";
 
 const PublicProfile = () => {
-    let { id } = useParams();
-    const [user, setUser] = useState({});
-    const [userPosts, setUserPosts] = useState([]);
-    const {
-        firstName,
-        lastName,
-        email,
-        mobile,
-        birthday,
-        location,
-        occupation,
-        image,
-    } = user;
-    console.log(userPosts);
+  let { id } = useParams();
+  const [user, setUser] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
 
-    useEffect(() => {
-        api.get(`/users/${id}`).then((res) => {
-            setUser(res.data.data);
-        });
+  useEffect(() => {
+    api.get(`/users/${id}`).then((res) => {
+      setUser(res.data.data);
+    });
 
-        api.get(`/posts/user/${id}`).then((res) => {
-            setUserPosts(res.data.data);
-        });
-    }, []);
-    return (
-        <div className='h-full p-8'>
-            <div className='bg-neutral-800 rounded-lg shadow-xl pb-8'>
-                <div className='w-full h-[250px]'>
-                    <img
-                        src='https://vojislavd.com/ta-template-demo/assets/img/profile-background.jpg'
-                        className='w-full h-full rounded-tl-lg rounded-tr-lg'
-                    />
-                </div>
-                <div className='flex flex-col items-center -mt-20'>
-                    <img
-                        src={image?.url}
-                        className='w-40 border-4 border-neutral-700 border-neutral-600 rounded-full'
-                    />
-                    <div className='flex items-center space-x-2 mt-2'>
-                        <p className='text-2xl'>
-                            {firstName} {lastName}
-                        </p>
-                        <span
-                            className='bg-blue-500 rounded-full p-1'
-                            title='Verified'
-                        >
-                            <svg
-                                xmlns='http://www.w3.org/2000/svg'
-                                className='text-gray-100 h-2.5 w-2.5'
-                                fill='none'
-                                viewBox='0 0 24 24'
-                                stroke='currentColor'
-                            >
-                                <path
-                                    strokeLinecap='round'
-                                    strokeLinejoin='round'
-                                    strokeWidth='4'
-                                    d='M5 13l4 4L19 7'
-                                ></path>
-                            </svg>
-                        </span>
-                    </div>
-                    <p className='text-neutral-400'>{email}</p>
-                    <p className='text-sm text-neutral-400'>{occupation}</p>
-                    <p className='text-sm text-neutral-400'>{location}</p>
-                    <p className='text-sm text-neutral-400'>{birthday}</p>
-                    <p className='text-sm text-neutral-400'>{mobile}</p>
-                </div>
-                <div className='flex-1 flex flex-col items-center lg:items-end justify-end px-8 mt-2'>
-                    <div className='flex items-center space-x-4 mt-2'>
-                        <button className='flex items-center bg-indigo-700 hover:bg-indigo-600 text-gray-100 px-4 py-2 rounded text-sm space-x-2 transition duration-100'>
-                            <svg
-                                xmlns='http://www.w3.org/2000/svg'
-                                className='h-4 w-4'
-                                viewBox='0 0 20 20'
-                                fill='currentColor'
-                            >
-                                <path d='M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z'></path>
-                            </svg>
-                            <span>Add Friend</span>
-                        </button>
-                    </div>
-                </div>
-            </div>
+    api.get(`/posts/user/${id}`).then((res) => {
+      setUserPosts(res.data.data);
+    });
+  }, [id]);
+
+  console.log(userPosts);
+  return (
+    <>
+      <Header />
+      <div className="w-5/6 mx-auto flex flex-col md:flex-row items-start gap-6 mt-10">
+        <div className="w-full md:w-4/12 lg:w-3/12 lg:max-w-xs">
+          <UserInfo user={user} />
         </div>
-    );
+
+        <div className="w-full md:w-8/12 lg:w-6/12 lg:max-w-xl">
+          <PostList posts={userPosts} />
+        </div>
+        <div className="w-full md:hidden lg:block lg:w-3/12 lg:max-w-xs">
+          {/* TODO: advertisements, friends list */}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PublicProfile;
