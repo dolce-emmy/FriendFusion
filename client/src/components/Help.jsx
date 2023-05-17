a75b6eee1546d18903b89103b97970a22b77a1b4
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-17 14:04:52 +0200

    updating all the Header icons and replacing them by the heroicons

--- client/src/components/Help.jsx
+++ client/src/components/Help.jsx
@@ -15,10 +15,7 @@ function Help() {
 
     return (
         <div>
-            <Header
-                isNightMode={isNightMode}
-                onToggleNightMode={handleToggleNightMode}
-            />
+            <Header />
             <div className='max-w-2xl mx-auto py-8 px-4'>
                 <h2 className='text-3xl font-bold mb-8'>Help Center</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,32 +1,31 @@
-import React from "react";
-import UserInfo from "./UserInfo";
-import Header from "./Header";
-import PostForm from "./PostForm";
-import PostList from "./PostList";
-import { useAppContext } from "../context/AppContext";
+import React from 'react';
+import UserInfo from './UserInfo';
+import Header from './Header';
+import PostForm from './PostForm';
+import PostList from './PostList';
+import { useAppContext } from '../context/AppContext';
 
 const Home = () => {
-  const { user, posts } = useAppContext();
-  return (
-    <div>
-      {/* Header */}
-      <Header />
-      <div className="w-5/6 mx-auto flex flex-col md:flex-row items-start gap-6 mt-10">
-        <div className="w-full md:w-4/12 lg:w-3/12 lg:max-w-xs">
-          <UserInfo user={user} />
-        </div>
+    const { user, posts } = useAppContext();
+    return (
+        <div>
+            <Header />
+            <div className='w-5/6 mx-auto flex flex-col md:flex-row items-start gap-6 mt-10'>
+                <div className='w-full md:w-4/12 lg:w-3/12 lg:max-w-xs'>
+                    <UserInfo user={user} />
+                </div>
 
-        <div className="w-full md:w-8/12 lg:w-6/12 lg:max-w-xl">
-          <PostForm />
-          <PostList posts={posts} />
+                <div className='w-full md:w-8/12 lg:w-6/12 lg:max-w-xl'>
+                    <PostForm />
+                    <PostList posts={posts} />
+                </div>
+                <div className='w-full md:hidden lg:block lg:w-3/12 lg:max-w-xs'>
+                    {/* TODO: advertisements, friends list */}
+                </div>
+            </div>
+            {/* Footer */}
         </div>
-        <div className="w-full md:hidden lg:block lg:w-3/12 lg:max-w-xs">
-          {/* TODO: advertisements, friends list */}
-        </div>
-      </div>
-      {/* Footer */}
-    </div>
-  );
+    );
 };
 
 export default Home;
