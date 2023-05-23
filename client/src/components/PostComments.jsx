1417617611770a6646a9b3d059446b4d015011cb
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-23 17:42:19 +0200

    updating the styling for the comment and alert components

--- client/src/components/PostComments.jsx
+++ client/src/components/PostComments.jsx
@@ -1,101 +1,105 @@
-import React, { useState } from "react";
-import { useAppContext } from "../context/AppContext";
-import UserBasicInfo from "./UserBasicInfo";
-import api from "../api";
-import InputEmoji from "react-input-emoji";
+import React, { useState } from 'react';
+import { useAppContext } from '../context/AppContext';
+import UserBasicInfo from './UserBasicInfo';
+import api from '../api';
+import InputEmoji from 'react-input-emoji';
 
 const PostComment = ({ postId, comment, onDeleteComment }) => {
-  const { _id, content, user, createdAt } = comment;
-  const { user: currentUser, handleDeleteCommentsForPost } = useAppContext();
+    const { _id, content, user, createdAt } = comment;
+    const { user: currentUser, handleDeleteCommentsForPost } = useAppContext();
 
-  // after that we need to delete the comments from the backend
-  // and then we need to update the comments in the state of the post page
-  const handleDeleteComment = (id) => {
-    api
-      .post(`/comments/${id}/post/${postId}`)
-      .then((res) => {
-        if (res.data.success) {
-          handleDeleteCommentsForPost(postId, res.data.data);
-          onDeleteComment();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+    // after that we need to delete the comments from the backend
+    // and then we need to update the comments in the state of the post page
+    const handleDeleteComment = (id) => {
+        api.post(`/comments/${id}/post/${postId}`)
+            .then((res) => {
+                if (res.data.success) {
+                    handleDeleteCommentsForPost(postId, res.data.data);
+                    onDeleteComment();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-  const extraInfo = (
-    <>
-      <span>{content}</span>
-      {user?._id === currentUser._id && (
-        <span
-          className="block text-sm text-neutral-500 cursor-pointer"
-          onClick={() => handleDeleteComment(_id)}
-        >
-          Remove
-        </span>
-      )}
-    </>
-  );
+    const extraInfo = (
+        <>
+            <span>{content}</span>
+            {user?._id === currentUser._id && (
+                <span
+                    className='block text-sm text-neutral-500 cursor-pointer'
+                    onClick={() => handleDeleteComment(_id)}
+                >
+                    Remove
+                </span>
+            )}
+        </>
+    );
 
-  return (
-    <div key={_id}>
-      <UserBasicInfo user={user} extraInfo={extraInfo} timeStamp={createdAt} />
-    </div>
-  );
+    return (
+        <div key={_id}>
+            <UserBasicInfo
+                user={user}
+                extraInfo={extraInfo}
+                timeStamp={createdAt}
+            />
+        </div>
+    );
 };
 
 const PostComments = (post) => {
-  const { _id, comments, populatedComments, getComments } = post;
-  const { handleAddCommentsForPost } = useAppContext();
-  const [comment, setComment] = useState("");
+    const { _id, comments, populatedComments, getComments } = post;
+    const { handleAddCommentsForPost } = useAppContext();
+    const [comment, setComment] = useState('');
 
-  const handleComment = (content) => {
-    const data = new FormData();
-    data.append("content", content);
+    const handleComment = (content) => {
+        const data = new FormData();
+        data.append('content', content);
 
-    api
-      .post(`/comments/${_id}`, data, {
-        headers: { "Content-Type": "application/json" },
-      })
-      .then((res) => {
-        if (res.data.success) {
-          const cmt = res.data.data;
-          handleAddCommentsForPost(_id, cmt);
-          getComments([...comments, cmt._id]);
-          setComment("");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+        api.post(`/comments/${_id}`, data, {
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then((res) => {
+                if (res.data.success) {
+                    const cmt = res.data.data;
+                    handleAddCommentsForPost(_id, cmt);
+                    getComments([...comments, cmt._id]);
+                    setComment('');
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
-  return (
-    <>
-      <div className="mr-2 pl-2">
-        <InputEmoji
-          value={comment}
-          onChange={setComment}
-          cleanOnEnter
-          onEnter={handleComment}
-          placeholder="Write a comment"
-        />
-      </div>
-      <div className="flex flex-col gap-4 px-4 mt-5 mb-3">
-        {populatedComments?.map((comment) => (
-          <PostComment
-            key={comment._id}
-            postId={_id}
-            comment={comment}
-            onDeleteComment={() =>
-              getComments(comments?.filter((c) => c._id !== comment._id))
-            }
-          />
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className='mr-2 pl-2'>
+                <InputEmoji
+                    value={comment}
+                    onChange={setComment}
+                    cleanOnEnter
+                    onEnter={handleComment}
+                    placeholder='Write a comment'
+                />
+            </div>
+            <div className='flex flex-col gap-4 px-4 mt-5 mb-3'>
+                {populatedComments?.map((comment) => (
+                    <PostComment
+                        key={comment._id}
+                        postId={_id}
+                        comment={comment}
+                        onDeleteComment={() =>
+                            getComments(
+                                comments?.filter((c) => c._id !== comment._id)
+                            )
+                        }
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default PostComments;
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,243 +1,259 @@
-import UserCollection from "../models/userSchema.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import UserCollection from '../models/userSchema.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async (req, res) => {
-  try {
-    const user = new UserCollection(req.body);
-    const hashedPassword = bcrypt.hashSync(user.password, 10);
-    user.password = hashedPassword;
-    await user.save();
-    res.status(201).json({ success: true, data: user });
-  } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
-  }
+    try {
+        const user = new UserCollection(req.body);
+        const hashedPassword = bcrypt.hashSync(user.password, 10);
+        user.password = hashedPassword;
+        await user.save();
+        res.status(201).json({ success: true, data: user });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
 };
 
 export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await UserCollection.findOne({ email }).populate("friends image");
-    if (user) {
-      const verifyPassword = bcrypt.compareSync(password, user.password);
-      if (verifyPassword) {
-        const token = jwt.sign(
-          { id: user._id, email: user.email },
-          process.env.JWT_SECRET,
-          { expiresIn: "1h" }
+    try {
+        const { email, password } = req.body;
+        const user = await UserCollection.findOne({ email }).populate(
+            'friends image'
         );
-        res.header("token", token).json({ success: true, data: user });
-      } else {
-        res.status(403).json({
-          success: false,
-          data: "Invalid password",
-        });
-      }
-    } else {
-      res.status(403).json({ success: false, data: "Invalid email" });
+        if (user) {
+            const verifyPassword = bcrypt.compareSync(password, user.password);
+            if (verifyPassword) {
+                const token = jwt.sign(
+                    { id: user._id, email: user.email },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '1h' }
+                );
+                res.header('token', token).json({ success: true, data: user });
+            } else {
+                res.status(403).json({
+                    success: false,
+                    data: 'Invalid password',
+                });
+            }
+        } else {
+            res.status(403).json({ success: false, data: 'Invalid email' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, data: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, data: error.message });
-  }
 };
 
 export const getAllUsers = async (req, res) => {
-  try {
-    const users = await UserCollection.find();
-    res.status(200).json({ success: true, data: users });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+    try {
+        const users = await UserCollection.find();
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
 export const refreshPage = async (req, res) => {
-  res.json({ success: true, data: req.user });
+    res.json({ success: true, data: req.user });
 };
 
 export const getSingleUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await UserCollection.findById(id).populate([
-      {
-        path: "image",
-        model: "Image",
-      },
-      {
-        path: "friends",
-        model: "User",
-        populate: {
-          path: "image",
-          model: "Image",
-        },
-      },
-    ]);
-    if (user) {
-      res.status(200).json({ success: true, data: user });
-    } else {
-      res.status(404).json({ success: false, message: "User not found" });
+    try {
+        const { id } = req.params;
+        const user = await UserCollection.findById(id).populate([
+            {
+                path: 'image',
+                model: 'Image',
+            },
+            {
+                path: 'friends',
+                model: 'User',
+                populate: {
+                    path: 'image',
+                    model: 'Image',
+                },
+            },
+        ]);
+        if (user) {
+            res.status(200).json({ success: true, data: user });
+        } else {
+            res.status(404).json({ success: false, message: 'User not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
 };
 
 export const updateUser = async (req, res) => {
-  try {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    const {
-      image,
-      firstName,
-      lastName,
-      occupation,
-      location,
-      birthday,
-      mobile,
-    } = req.body;
+        const {
+            image,
+            firstName,
+            lastName,
+            occupation,
+            location,
+            birthday,
+            mobile,
+        } = req.body;
 
-    const updateUser = await UserCollection.findByIdAndUpdate(
-      id,
-      { image, firstName, lastName, occupation, location, birthday, mobile },
-      {
-        new: true,
-      }
-    );
+        const updateUser = await UserCollection.findByIdAndUpdate(
+            id,
+            {
+                image,
+                firstName,
+                lastName,
+                occupation,
+                location,
+                birthday,
+                mobile,
+            },
+            {
+                new: true,
+            }
+        );
 
-    res.status(200).json({ success: true, data: updateUser });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+        res.status(200).json({ success: true, data: updateUser });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
 export const deleteUser = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedUser = await UserCollection.findByIdAndDelete(id);
-    res.status(200).json({ success: true, data: deletedUser });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+    try {
+        const { id } = req.params;
+        const deletedUser = await UserCollection.findByIdAndDelete(id);
+        res.status(200).json({ success: true, data: deletedUser });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
 export const addFriend = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { friendId } = req.body;
-    const user = await UserCollection.findById(id);
-    if (!user) {
-      res.status(404).json({ success: false, message: "User not found" });
-    } else {
-      const friend = await UserCollection.findById(friendId);
-      if (!friend) {
-        res.status(404).json({ success: false, message: "Friend not found" });
-      } else {
-        const isFriend = user.friends.includes(friendId);
-        if (isFriend) {
-          res
-            .status(400)
-            .json({ success: false, message: "Friend already added" });
+    try {
+        const { id } = req.params;
+        const { friendId } = req.body;
+        const user = await UserCollection.findById(id);
+        if (!user) {
+            res.status(404).json({ success: false, message: 'User not found' });
         } else {
-          user.friends.push(friendId);
-          const updatedUser = await user.save();
-          const populatedUser = await UserCollection.findById(
-            updatedUser._id
-          ).populate([
-            {
-              path: "image",
-              model: "Image",
-            },
-            {
-              path: "friends",
-              model: "User",
-              populate: {
-                path: "image",
-                model: "Image",
-              },
-            },
-          ]);
-          res.status(201).json({
-            success: true,
+            const friend = await UserCollection.findById(friendId);
+            if (!friend) {
+                res.status(404).json({
+                    success: false,
+                    message: 'Friend not found',
+                });
+            } else {
+                const isFriend = user.friends.includes(friendId);
+                if (isFriend) {
+                    res.status(400).json({
+                        success: false,
+                        message: 'Friend already added',
+                    });
+                } else {
+                    user.friends.push(friendId);
+                    const updatedUser = await user.save();
+                    const populatedUser = await UserCollection.findById(
+                        updatedUser._id
+                    ).populate([
+                        {
+                            path: 'image',
+                            model: 'Image',
+                        },
+                        {
+                            path: 'friends',
+                            model: 'User',
+                            populate: {
+                                path: 'image',
+                                model: 'Image',
+                            },
+                        },
+                    ]);
+                    res.status(201).json({
+                        success: true,
 
-            data: populatedUser,
-          });
+                        data: populatedUser,
+                    });
+                }
+            }
         }
-      }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
 };
 
 export const removeFriend = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { friendId } = req.body;
-    const user = await UserCollection.findById(id);
-    if (!user) {
-      res.status(404).json({ success: false, message: "User not found" });
-    } else {
-      const isFriend = user.friends.includes(friendId);
-      if (!isFriend) {
-        res.status(400).json({ success: false, message: "Friend not found" });
-      } else {
-        user.friends = user.friends.filter(
-          (friend) => friend.toString() !== friendId.toString()
-        );
-        const updatedUser = await user.save();
-        const populatedUser = await UserCollection.findById(
-          updatedUser._id
-        ).populate([
-          {
-            path: "image",
-            model: "Image",
-          },
-          {
-            path: "friends",
-            model: "User",
-            populate: {
-              path: "image",
-              model: "Image",
-            },
-          },
-        ]);
-        res.status(200).json({
-          success: true,
+    try {
+        const { id } = req.params;
+        const { friendId } = req.body;
+        const user = await UserCollection.findById(id);
+        if (!user) {
+            res.status(404).json({ success: false, message: 'User not found' });
+        } else {
+            const isFriend = user.friends.includes(friendId);
+            if (!isFriend) {
+                res.status(400).json({
+                    success: false,
+                    message: 'Friend not found',
+                });
+            } else {
+                user.friends = user.friends.filter(
+                    (friend) => friend.toString() !== friendId.toString()
+                );
+                const updatedUser = await user.save();
+                const populatedUser = await UserCollection.findById(
+                    updatedUser._id
+                ).populate([
+                    {
+                        path: 'image',
+                        model: 'Image',
+                    },
+                    {
+                        path: 'friends',
+                        model: 'User',
+                        populate: {
+                            path: 'image',
+                            model: 'Image',
+                        },
+                    },
+                ]);
+                res.status(200).json({
+                    success: true,
 
-          data: populatedUser,
-        });
-      }
+                    data: populatedUser,
+                });
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
 };
 
 export const getUserFriends = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await UserCollection.findById(id).populate("friends");
-    if (!user) {
-      res.status(404).json({ success: false, message: "User not found" });
-    } else {
-      res.status(200).json({ success: true, data: user.friends });
+    try {
+        const { id } = req.params;
+        const user = await UserCollection.findById(id).populate('friends');
+        if (!user) {
+            res.status(404).json({ success: false, message: 'User not found' });
+        } else {
+            res.status(200).json({ success: true, data: user.friends });
+        }
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
 };
 
 export const searchUsers = async (req, res) => {
-  const { query: searchTerm } = req.body;
-  try {
-    const users = await UserCollection.find({
-      $or: [
-        { firstName: { $regex: searchTerm, $options: "i" } },
-        { lastName: { $regex: searchTerm, $options: "i" } },
-        { email: { $regex: searchTerm, $options: "i" } },
-      ],
-    }).populate("image");
-    res.status(200).json({ success: true, data: users });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
-  }
+    const { query: searchTerm } = req.body;
+    try {
+        const users = await UserCollection.find({
+            $or: [
+                { firstName: { $regex: searchTerm, $options: 'i' } },
+                { lastName: { $regex: searchTerm, $options: 'i' } },
+            ],
+        }).populate('image');
+        res.status(200).json({ success: true, data: users });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
