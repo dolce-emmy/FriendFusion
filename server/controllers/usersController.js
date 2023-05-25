28e6a69239a50bd664e63d40d31f2f86980ed462
Mohammed Al-shameri <malshameri54@gmail.com>
2023-05-25 15:31:26 +0200

    updating the  reset password in the router and the controller and in the schema and in the client side too

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,6 +1,10 @@
-import UserCollection from '../models/userSchema.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import UserCollection from "../models/userSchema.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import {
+    generateResetToken,
+    sendPasswordResetEmail,
+} from "../utils/resetPassword.js";
 
 export const registerUser = async (req, res) => {
     try {
@@ -18,7 +22,7 @@ export const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
         const user = await UserCollection.findOne({ email }).populate(
-            'friends image'
+            "friends image"
         );
         if (user) {
             const verifyPassword = bcrypt.compareSync(password, user.password);
@@ -26,13 +30,13 @@ export const loginUser = async (req, res) => {
                 const token = jwt.sign(
                     { id: user._id, email: user.email },
                     process.env.JWT_SECRET,
-                    { expiresIn: '1h' }
+                    { expiresIn: "1h" }
                 );
-                res.header('token', token).json({ success: true, data: user });
+                res.header("token", token).json({ success: true, data: user });
             } else {
                 res.status(403).json({
-                  success: false,
-                  message: "Invalid password",
+                    success: false,
+                    message: "Invalid password",
                 });
             }
         } else {
@@ -61,22 +65,22 @@ export const getSingleUser = async (req, res) => {
         const { id } = req.params;
         const user = await UserCollection.findById(id).populate([
             {
-                path: 'image',
-                model: 'Image',
+                path: "image",
+                model: "Image",
             },
             {
-                path: 'friends',
-                model: 'User',
+                path: "friends",
+                model: "User",
                 populate: {
-                    path: 'image',
-                    model: 'Image',
+                    path: "image",
+                    model: "Image",
                 },
             },
         ]);
         if (user) {
             res.status(200).json({ success: true, data: user });
         } else {
-            res.status(404).json({ success: false, message: 'User not found' });
+            res.status(404).json({ success: false, message: "User not found" });
         }
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
@@ -135,20 +139,20 @@ export const addFriend = async (req, res) => {
         const { friendId } = req.body;
         const user = await UserCollection.findById(id);
         if (!user) {
-            res.status(404).json({ success: false, message: 'User not found' });
+            res.status(404).json({ success: false, message: "User not found" });
         } else {
             const friend = await UserCollection.findById(friendId);
             if (!friend) {
                 res.status(404).json({
                     success: false,
-                    message: 'Friend not found',
+                    message: "Friend not found",
                 });
             } else {
                 const isFriend = user.friends.includes(friendId);
                 if (isFriend) {
                     res.status(400).json({
                         success: false,
-                        message: 'Friend already added',
+                        message: "Friend already added",
                     });
                 } else {
                     user.friends.push(friendId);
@@ -157,15 +161,15 @@ export const addFriend = async (req, res) => {
                         updatedUser._id
                     ).populate([
                         {
-                            path: 'image',
-                            model: 'Image',
+                            path: "image",
+                            model: "Image",
                         },
                         {
-                            path: 'friends',
-                            model: 'User',
+                            path: "friends",
+                            model: "User",
                             populate: {
-                                path: 'image',
-                                model: 'Image',
+                                path: "image",
+                                model: "Image",
                             },
                         },
                     ]);
@@ -188,13 +192,13 @@ export const removeFriend = async (req, res) => {
         const { friendId } = req.body;
         const user = await UserCollection.findById(id);
         if (!user) {
-            res.status(404).json({ success: false, message: 'User not found' });
+            res.status(404).json({ success: false, message: "User not found" });
         } else {
             const isFriend = user.friends.includes(friendId);
             if (!isFriend) {
                 res.status(400).json({
                     success: false,
-                    message: 'Friend not found',
+                    message: "Friend not found",
                 });
             } else {
                 user.friends = user.friends.filter(
@@ -205,15 +209,15 @@ export const removeFriend = async (req, res) => {
                     updatedUser._id
                 ).populate([
                     {
-                        path: 'image',
-                        model: 'Image',
+                        path: "image",
+                        model: "Image",
                     },
                     {
-                        path: 'friends',
-                        model: 'User',
+                        path: "friends",
+                        model: "User",
                         populate: {
-                            path: 'image',
-                            model: 'Image',
+                            path: "image",
+                            model: "Image",
                         },
                     },
                 ]);
@@ -232,9 +236,9 @@ export const removeFriend = async (req, res) => {
 export const getUserFriends = async (req, res) => {
     try {
         const { id } = req.params;
-        const user = await UserCollection.findById(id).populate('friends');
+        const user = await UserCollection.findById(id).populate("friends");
         if (!user) {
-            res.status(404).json({ success: false, message: 'User not found' });
+            res.status(404).json({ success: false, message: "User not found" });
         } else {
             res.status(200).json({ success: true, data: user.friends });
         }
@@ -248,12 +252,68 @@ export const searchUsers = async (req, res) => {
     try {
         const users = await UserCollection.find({
             $or: [
-                { firstName: { $regex: searchTerm, $options: 'i' } },
-                { lastName: { $regex: searchTerm, $options: 'i' } },
+                { firstName: { $regex: searchTerm, $options: "i" } },
+                { lastName: { $regex: searchTerm, $options: "i" } },
             ],
-        }).populate('image');
+        }).populate("image");
         res.status(200).json({ success: true, data: users });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
+    }
+};
+
+// Controller for handling forgot password request
+export const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        // Check if the user with the provided email exists
+        const user = await UserCollection.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ error: "User not found" });
+        }
+
+        // Generate a password reset token and save it to the user's document
+        const resetToken = generateResetToken();
+        user.resetToken = resetToken;
+        user.resetTokenExpiry = Date.now() + 3600000; // Token expires in 1 hour
+        await user.save();
+
+        // Send password reset email to the user's email address
+        sendPasswordResetEmail(user.email, resetToken);
+
+        res.status(200).json({ message: "Password reset email sent" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+// Controller for handling password reset request
+export const resetPassword = async (req, res) => {
+    const { token, newPassword } = req.body;
+
+    try {
+        // Find the user with the provided reset token
+        const user = await UserCollection.findOne({
+            resetToken: token,
+            resetTokenExpiry: { $gt: Date.now() }, // Check if the token is not expired
+        });
+
+        if (!user) {
+            return res.status(400).json({ error: "Invalid or expired token" });
+        }
+
+        // Update the user's password and clear the reset token fields
+        user.password = newPassword;
+        user.resetToken = undefined;
+        user.resetTokenExpiry = undefined;
+        await user.save();
+
+        res.status(200).json({ message: "Password reset successfully" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal server error" });
     }
 };
--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,59 +1,61 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema(
-  {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      require: true,
-    },
-    location: {
-      type: String,
-    },
-    occupation: {
-      type: String,
-    },
-    mobile: {
-      type: String,
-    },
-    birthday: {
-      type: Date,
-    },
-    image: {
-      type: Schema.Types.ObjectId,
-      ref: "Image",
-    },
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            require: true,
+        },
+        resetToken: String,
+        resetTokenExpiry: Date,
+        location: {
+            type: String,
+        },
+        occupation: {
+            type: String,
+        },
+        mobile: {
+            type: String,
+        },
+        birthday: {
+            type: Date,
+        },
+        image: {
+            type: Schema.Types.ObjectId,
+            ref: "Image",
+        },
 
-    followers: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
+        followers: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
 
-    viewedProfile: {
-      type: Number,
+        viewedProfile: {
+            type: Number,
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 userSchema.indexes({ email: 1 });
