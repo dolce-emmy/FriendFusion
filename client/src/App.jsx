bd073b7b754ea24c8b1c67e546c997d07a056508
Mohammedalshameri <107537369+Mohammedalshameri@users.noreply.github.com>
2023-05-29 13:20:29 +0200

    Completing the reset password components with the server controller and router (#114)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,48 @@
-import './App.css';
-import AppContextProvider from './context/AppContext';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Register from './components/Register';
-import Login from './components/Login';
+import "./App.css";
+import AppContextProvider from "./context/AppContext";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 import ForgotPassword from "./components/ForgotPassword";
 import Profile from "./components/Profile";
 import Help from "./components/Help";
 import Comment from "./components/Comment";
 import Alert from "./components/Alert";
 import PublicProfile from "./components/PublicProfile";
-import NotFound from './components/NotFound';
+import NotFound from "./components/NotFound";
+import ResetPassword from "./components/ResetPassword";
 
 function App() {
-  return (
-    <AppContextProvider>
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/register" exact element={<Register />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/forgot-password" exact element={<ForgotPassword />} />
-        <Route path="/profile" exact element={<Profile />} />
-        <Route path="/profile/public/:id" exact element={<PublicProfile />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/comments" element={<Comment />} />
-        <Route path="/alerts" element={<Alert />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </AppContextProvider>
-  );
+    return (
+        <AppContextProvider>
+            <Routes>
+                <Route path="/" exact element={<Home />} />
+                <Route path="/register" exact element={<Register />} />
+                <Route path="/login" exact element={<Login />} />
+                <Route
+                    path="/forgot-password"
+                    exact
+                    element={<ForgotPassword />}
+                />
+                <Route
+                    path="/reset-password"
+                    exact
+                    element={<ResetPassword />}
+                />
+                <Route path="/profile" exact element={<Profile />} />
+                <Route
+                    path="/profile/public/:id"
+                    exact
+                    element={<PublicProfile />}
+                />
+                <Route path="/help" element={<Help />} />
+                <Route path="/comments" element={<Comment />} />
+                <Route path="/alerts" element={<Alert />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </AppContextProvider>
+    );
 }
 
 export default App;
