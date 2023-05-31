7604b6849866985e052a104bacf14fe58b691e26
rbinduc <107483540+rbinduc@users.noreply.github.com>
2023-05-31 17:15:32 +0200

    add logo and fav icon (#122)
    * add logo for site
    
    * add favicon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,6 +9,8 @@ import HelpIcon from "./icons/HelpIcon";
 import LogoutIcon from "./icons/LogoutIcon";
 import MoonIcon from "./icons/MoonIcon";
 import SunIcon from "./icons/SunIcon";
+import SearchIcon from "./icons/SearchIcon";
+import LogoIcon from "./icons/LogoIcon";
 
 const Header = () => {
   const { user } = useAppContext();
@@ -49,69 +51,81 @@ const Header = () => {
   };
 
   return (
-    <header className="flex justify-between items-center bg-neutral-800 shadow-md p-5 h-20">
-      <div className="left">
-        <Link to="/">
-          <h1 className="mr-5 font-medium hover:text-gray-900">FriendFusion</h1>
-        </Link>
-        <div className="relative">
-          <form onSubmit={handleSearch}>
-            <input type="text" name="search" placeholder="Search" />
-            <button
-              type="submit"
-              className="mr-5 font-medium hover:text-gray-900"
-            >
-              Search
-            </button>
-          </form>
-          {showSearchedUsers && (
-            <div
-              onClick={() => setShowSearchedUsers(false)}
-              className="fixed inset-0 h-full w-full z-10"
-            ></div>
-          )}
-          <div
-            className={`${
-              showSearchedUsers
-                ? "opacity-1 visible translate-y-0 scale-100"
-                : "opacity-0 invisible"
-            } absolute right-0 mt-2 py-2 w-full text-white bg-neutral-700 rounded-md shadow-xl z-20 transition-all duration-300 transform origin-top-right -translate-y-2 scale-95`}
-          >
-            {searchedUsers?.map((searchedUser) => (
-              <div
-                key={searchedUser._id}
-                className=" hover:bg-neutral-600 hover:text-white"
-              >
-                <UserBasicInfo
-                  handleOnClick={() => setShowSearchedUsers(false)}
-                  user={searchedUser}
+    <header className="bg-neutral-800 shadow-md p-5">
+      <div className="max-w-screen-2xl mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <h1>
+            <Link to="/" className="flex items-center gap-2">
+              {/* <img src="./logo.svg" className="w-20 h-20" /> */}
+              <LogoIcon />
+              <span className="text-xl font-semibold">Friend Fusion</span>
+            </Link>
+          </h1>
+          <div className="relative ml-10">
+            <form onSubmit={handleSearch} className="ml-5">
+              <div className="relative">
+                <input
+                  type="text"
+                  name="search"
+                  placeholder="Search"
+                  className="px-3 py-2 rounded-full focus:outline-none focus:border-indigo-500"
                 />
+                <button
+                  type="submit"
+                  className="absolute right-0.5 top-0.5 p-1.5 text-center bg-neutral-600 hover:bg-neutral-500 rounded-full transition-all duration-300"
+                >
+                  <SearchIcon />
+                </button>
               </div>
-            ))}
+            </form>
+            {showSearchedUsers && (
+              <div
+                onClick={() => setShowSearchedUsers(false)}
+                className="fixed inset-0 h-full w-full z-10"
+              ></div>
+            )}
+            <div
+              className={`${
+                showSearchedUsers
+                  ? "opacity-1 visible translate-y-0 scale-100"
+                  : "opacity-0 invisible"
+              } absolute right-0 mt-2 py-2 w-72 flex flex-col text-white bg-neutral-700 rounded-md shadow-xl z-20 transition-all duration-300 transform origin-top-right -translate-y-2 scale-95`}
+            >
+              {searchedUsers?.map((searchedUser) => (
+                <div
+                  key={searchedUser._id}
+                  className=" hover:bg-neutral-600 hover:text-white px-4 py-2"
+                >
+                  <UserBasicInfo
+                    handleOnClick={() => setShowSearchedUsers(false)}
+                    user={searchedUser}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+        <div className="flex items-center gap-2">
+          {isNightMode ? (
+            <MoonIcon onClick={handleToggleNightMode} />
+          ) : (
+            <SunIcon onClick={handleToggleNightMode} />
+          )}
 
-      <div className="right flex gap-2">
-        {isNightMode ? (
-          <MoonIcon onClick={handleToggleNightMode} />
-        ) : (
-          <SunIcon onClick={handleToggleNightMode} />
-        )}
-
-        <Link to="/comments">
-          <CommentIcon />
-        </Link>
-        <Link to="/alerts">
-          <AlertIcon />
-        </Link>
-        <Link to="/help">
-          <HelpIcon />
-        </Link>
-        <span>Welcome {user?.firstName}</span>
-        <button onClick={handleLogout}>
-          <LogoutIcon />
-        </button>
+          <Link to="/comments">
+            <CommentIcon />
+          </Link>
+          <Link to="/alerts">
+            <AlertIcon />
+          </Link>
+          <Link to="/help">
+            <HelpIcon />
+          </Link>
+          <span>Welcome {user?.firstName}</span>
+          <button onClick={handleLogout}>
+            <LogoutIcon />
+          </button>
+        </div>
       </div>
     </header>
   );
