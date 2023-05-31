d09aa9acc96e7a2e277039509d99fbbc75057ef8
rbinduc <107483540+rbinduc@users.noreply.github.com>
2023-05-31 09:46:54 +0200

    refactor profile ui design (#120)

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -2,7 +2,8 @@ import { useAppContext } from "../context/AppContext";
 import { useState } from "react";
 import api from "../api";
 import { useNavigate, Link } from "react-router-dom";
-import ArrowHomePageIcon from "./icons/ArrowHomePageIcon";
+import ArrowLeftIcon from "./icons/ArrowLeftIcon";
+import Header from "./Header";
 
 const Profile = () => {
   const { user, setUser } = useAppContext();
@@ -36,14 +37,7 @@ const Profile = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const formData = new FormData();
-    const file = e.target.file.files[0];
-    formData.append("file", file, file.name);
-
-    const imgRes = await api.post("/images", formData);
-    if (imgRes.data.success) {
-      const data = new FormData(e.target);
-      data.append("image", imgRes.data.data._id);
+    const updateUser = (data) => {
       api
         .patch(`/users/${user._id}`, data)
         .then((res) => {
@@ -62,7 +56,19 @@ const Profile = () => {
         .catch((err) => {
           setError(true);
         });
+    };
+
+    const data = new FormData(e.target);
+    const file = e.target.file.files[0];
+    if (file) {
+      const imageData = new FormData();
+      imageData.append("file", file, file.name);
+      const imgRes = await api.post("/images", imageData);
+      if (imgRes.data.success) {
+        data.append("image", imgRes.data.data._id);
+      }
     }
+    updateUser(data);
   };
 
   // Showing success message
@@ -94,129 +100,127 @@ const Profile = () => {
   };
 
   return (
-    <div className="w-full h-full max-w-sm mx-auto rounded-md shadow-md bg-neutral-800 p-8">
-      <div>
-        <h1 className="text-3xl font-bold text-center">Edit Your Profile</h1>
-      </div>
-      <br />
-
-      <div className="flex justify-center items-center">
-        <img
-          alt="..."
-          src={user?.image?.url}
-          className="shadow-xl rounded-full h-20 w-20 align-middle border-none  -m-16 my-0.5  -ml-30 lg:-ml-16 max-w-250-px"
-        />
-      </div>
-      <br />
-      <form className="mb-4" onSubmit={handleSubmit}>
-        <label className="block text-sm font-bold mb-2">First Name</label>
-        <input
-          name="firstName"
-          defaultValue={user?.firstName}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.firstName}
-          type="text"
-        />
-        <br />
-        <br />
-        <label className="label block text-sm font-bold mb-2">Last Name</label>
-        <input
-          name="lastName"
-          defaultValue={user?.lastName}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.lastName}
-          type="text"
-        />
-        <br />
-        <br />
-        <label className="label block text-sm font-bold mb-2">Email</label>
-        <input
-          name="email"
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          defaultValue={user?.email}
-          //value={user?.email}
-          type="email"
-        />
-        <br />
-
-        <br />
-
-        <label className="label block text-sm font-bold mb-2">Occupation</label>
-        <input
-          name="occupation"
-          defaultValue={user?.occupation}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.occupation}
-          type="text"
-        />
-
-        <br />
-        <br />
-        <label className="label block text-sm font-bold mb-2">mobile</label>
-        <input
-          name="mobile"
-          defaultValue={user?.mobile}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.mobile}
-          type="tel"
-        />
-        <br />
-        <br />
-
-        <label className="label block text-sm font-bold mb-2 ">Location</label>
-        <input
-          name="location"
-          defaultValue={user?.location}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.location}
-          type="text"
-        />
-
-        <br />
-        <br />
-
-        <label className="label block text-sm font-bold mb-2">Birthday</label>
-        <input
-          name="birthday"
-          defaultValue={user?.birthday}
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.birthday}
-          type="date"
-        />
-        <br />
-        <br />
-
-        <label className="label block text-sm font-bold mb-2">
-          Profile Picture
-        </label>
-        <input
-          name="file"
-          // onChange={handleInputChange}
-          className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
-          //value={user?.picturePath}
-          type="file"
-        />
-
-        <br />
-        <br />
-
-        <button className="btn block w-full bg-indigo-700 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 mt-6">
-          Submit
-        </button>
-      </form>
-      <button className="btn block w-full bg-indigo-700 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 mt-6">
-        <Link to="/" className=" flex content-between justify-center">
-          HomePage <ArrowHomePageIcon />
+    <>
+      <Header />
+      <div className="w-full h-full max-w-4xl mx-auto rounded-md shadow-md bg-neutral-800 p-8 mt-10 relative">
+        <Link
+          to="/"
+          className="absolute left-3 top-3 bg-neutral-700 hover:bg-neutral-600 p-2 rounded-full"
+        >
+          <ArrowLeftIcon />
         </Link>
-      </button>
-    </div>
+
+        <h1 className="text-3xl font-bold text-center mb-10">
+          Edit Your Profile
+        </h1>
+        <section>
+          <form className="mb-4" onSubmit={handleSubmit}>
+            <div className="flex flex-col items-center gap-4 mb-8">
+              <img
+                alt={user?.firstName}
+                src={user?.image?.url}
+                className="shadow-xl rounded-full h-28 w-28 align-middle border-none -m-16 my-0.5 -ml-30 lg:-ml-16 max-w-250-px"
+              />
+              <label className="flex gap-1 cursor-pointer" htmlFor="image">
+                <span>Upload Photo</span>
+                <input
+                  type="file"
+                  id="image"
+                  name="file"
+                  className="hidden"
+                  accept="image/*"
+                />
+              </label>
+            </div>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label className="block text-sm font-bold mb-2">
+                  First Name
+                </label>
+                <input
+                  name="firstName"
+                  defaultValue={user?.firstName}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2">
+                  Last Name
+                </label>
+                <input
+                  name="lastName"
+                  defaultValue={user?.lastName}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2">
+                  Email
+                </label>
+                <input
+                  name="email"
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  defaultValue={user?.email}
+                  type="email"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2">
+                  Occupation
+                </label>
+                <input
+                  name="occupation"
+                  defaultValue={user?.occupation}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2">
+                  Mobile
+                </label>
+                <input
+                  name="mobile"
+                  defaultValue={user?.mobile}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="tel"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2 ">
+                  Location
+                </label>
+                <input
+                  name="location"
+                  defaultValue={user?.location}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label className="label block text-sm font-bold mb-2">
+                  Birthday
+                </label>
+                <input
+                  name="birthday"
+                  defaultValue={user?.birthday}
+                  className="input w-full px-3 py-2 rounded-md focus:outline-none focus:border-indigo-500"
+                  type="date"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-end mt-6">
+              <button className="bg-indigo-700 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300 mt-6">
+                Save
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </>
   );
 };
 
