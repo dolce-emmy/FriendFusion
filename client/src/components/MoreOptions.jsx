d2aab99ee435a2775d72f3f6ee2dafe8bbbf0125
rbinduc <107483540+rbinduc@users.noreply.github.com>
2023-05-31 09:46:26 +0200

    refactor profile ui design

--- client/src/components/MoreOptions.jsx
+++ client/src/components/MoreOptions.jsx
@@ -7,38 +7,9 @@ const MoreOptions = ({ handleDelete }) => {
 
   return (
     <>
-      {/* <button
-        onClick={() => setOpen(!open)}
-        className="absolute right-4 top-2 p-1 hover:bg-neutral-500 rounded-full"
-      >
-        <EllipsisIcon />
-      </button>
-      <div
-        className={`transition-all duration-300 transform origin-top-right -translate-y-2 scale-95 ${
-          open
-            ? `opacity-1 visible translate-x-0 translate-y-0 scale-100`
-            : `opacity-0 invisible`
-        }`}
-      >
-        <div
-          className="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg outline-none"
-          role="menu"
-        >
-          <button
-            onClick={handleDelete}
-            className="flex gap-1 text-sm items-center text-red-500 ml-auto"
-          >
-            <span>Delete</span>
-            <span>
-              <DeleteIcon />
-            </span>
-          </button>
-        </div>
-      </div> */}
-
       <button
         onClick={() => setOpen(!open)}
-        className={`absolute right-4 top-2 p-1 hover:bg-neutral-600 rounded-full ${
+        className={`absolute right-4 top-1 p-1 hover:bg-neutral-600 rounded-full ${
           open && "bg-neutral-600"
         }`}
       >
