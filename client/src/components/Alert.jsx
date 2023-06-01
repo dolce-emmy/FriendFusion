4300e3a8d7c3d71a2b4d2d4cd229a59ff303eea7
Mohammed Al-shameri <malshameri54@gmail.com>
2023-06-01 14:47:35 +0200

    completing the light dark mode for the whole components

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -1,102 +1,98 @@
-import React, { useState } from 'react';
-import Header from './Header.jsx';
+import React, { useState } from "react";
+import { useAppContext } from "../context/AppContext";
+import Header from "./Header.jsx";
+import SpinnerIcon from "./icons/SpinnerIcon";
+import { useThemeContext } from "../context/ThemeContext";
 
-const Alert = () => {
-    const [alert, setAlert] = useState('');
+const Alert = ({ toggleCommentForm }) => {
+    const { isDarkMode } = useThemeContext();
+    const [formData, setFormData] = useState({
+        question1: "",
+        question2: "",
+        question3: "",
+    });
+    const { user } = useAppContext();
+    const [loading, setLoading] = useState(false); // Assuming you have a loading state
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log('Alert submitted:', alert);
-        setAlert('');
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
     };
 
-    //     return (
-    //         <>
-    //             <Header />
-    //             <div className='relative'>
-    //                 <form
-    //                     className='absolute right-0 top-full bg-white rounded-lg shadow-md p-4 mt-2 mt-[40px]'
-    //                     style={{ left: '50%', transform: 'translateX(-50%)' }}
-    //                 >
-    //                     <div className='mb-2'>
-    //                         <label
-    //                             htmlFor='name'
-    //                             className='block text-gray-700 font-medium mb-1'
-    //                         >
-    //                             Name:
-    //                         </label>
-    //                         <input
-    //                             type='text'
-    //                             id='name'
-    //                             name='name'
-    //                             className='w-full border-gray-400 border rounded py-2 px-3'
-    //                         />
-    //                     </div>
-    //                     <div className='mb-2'>
-    //                         <label
-    //                             htmlFor='message'
-    //                             className='block text-gray-700 font-medium mb-1'
-    //                         >
-    //                             Message:
-    //                         </label>
-    //                         <textarea
-    //                             id='message'
-    //                             name='message'
-    //                             className='w-full border-gray-400 border rounded py-2 px-3'
-    //                         ></textarea>
-    //                     </div>
-    //                     <button
-    //                         type='submit'
-    //                         className='bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-3'
-    //                     >
-    //                         Create
-    //                     </button>
-    //                 </form>
-    //             </div>
-    //         </>
-    //     );
-    // };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        // Handle form submission here
+        console.log(formData);
+        setLoading(true); // Set loading state to true during form submission
 
-    // export default Alert;
+        // Simulating an asynchronous request with setTimeout
+        setTimeout(() => {
+            setLoading(false); // Set loading state to false after form submission
+            toggleCommentForm();
+        }, 2000); // Adjust the timeout as needed
+    };
 
     return (
-      <>
-        <Header />
-        <div className="relative">
-          <form
-            className="absolute right-0 border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg mt-[150px]"
-            style={{ left: "50%", transform: "translateX(-50%)" }}
-          >
-            <div className="mb-2">
-              <label htmlFor="name" className="text-xl font-bold mb-4">
-                Name:
-              </label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                className="w-full border-gray-400 border rounded py-2 px-3"
-              />
+        <>
+            <Header />
+            <div className="flex justify-center items-center h-screen mt-[-80px]">
+                <div
+                    className={`${
+                        isDarkMode ? "dark" : "light"
+                    } flex flex-col gap-4 items-center justify-center bg-neutral-500 bg-neutral-500 rounded-2xl p-4 shadow-md text-left`}
+                >
+                    {/* text-center text-xl font-semibold py-3 */}
+                    <div className="p-3 border-b">
+                        <h2 className="text-center text-xl font-semibold py-3">
+                            Add Your Alert
+                        </h2>
+                    </div>
+                    <form className="p-4" onSubmit={onSubmitHandler}>
+                        <div className="mb-4">
+                            <label
+                                className="text-xl font mb-4"
+                                htmlFor="question1"
+                            >
+                                Name:
+                            </label>
+                            <input
+                                className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+                                type="text"
+                                name="question1"
+                                id="question1"
+                                placeholder="Your Name"
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="mb-4">
+                            <label
+                                className="text-xl font mb-4"
+                                htmlFor="message"
+                            >
+                                Message:
+                            </label>
+                            <textarea
+                                className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
+                                name="message"
+                                id="message"
+                                placeholder="Enter your message"
+                                onChange={handleChange}
+                            />
+                        </div>
+
+                        <div className="flex justify-end pt-4">
+                            <button
+                                disabled={loading}
+                                onClick={onSubmitHandler}
+                                className="flex cursor-pointer ml-auto bg-indigo-700 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
+                            >
+                                {loading ? <SpinnerIcon /> : "Create"}
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className="mb-2">
-              <label htmlFor="message" className="text-xl font-bold mb-4">
-                Message:
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full border-gray-400 border rounded py-2 px-3"
-              ></textarea>
-            </div>
-            <button
-              type="submit"
-              className="bg-blue-500 hover:bg-blue-600 text-white rounded-lg py-2 px-3"
-            >
-              Create
-            </button>
-          </form>
-        </div>
-      </>
+        </>
     );
 };
 
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,215 +1,11 @@
-// import React, { useState } from "react";
-// import { useAppContext } from "../context/AppContext";
-// import Header from "./Header.jsx";
-
-// const Comment = ({ toggleCommentForm }) => {
-//     const [formData, setFormData] = useState({
-//         question1: "",
-//         question2: "",
-//         question3: "",
-//     });
-//     const { user } = useAppContext();
-
-//     const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         // Handle form submission here
-//         console.log(formData);
-//         toggleCommentForm();
-//     };
-
-//     return (
-//         <>
-//             <Header />
-//             <div className="flex justify-center items-center h-screen mt-[-80px]">
-//                 <div className="w-96 border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg">
-//                     <div className="p-4 border-b">
-//                         <h2 className="text-xl font-medium">Add Comment</h2>
-//                     </div>
-//                     <form className="p-4" onSubmit={handleSubmit}>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="text-xl font mb-4"
-//                                 htmlFor="question1"
-//                             >
-//                                 How Can we Help You?
-//                             </label>
-//                             <input
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 type="text"
-//                                 name="question1"
-//                                 id="question1"
-//                                 placeholder="Your Comments"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="text-xl font mb-4"
-//                                 htmlFor="question2"
-//                             >
-//                                 How Can we Improve
-//                             </label>
-//                             <input
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 type="text"
-//                                 name="question2"
-//                                 id="question2"
-//                                 placeholder="Your Comments"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="text-xl font mb-4"
-//                                 htmlFor="message"
-//                             >
-//                                 Details
-//                             </label>
-//                             <textarea
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 name="message"
-//                                 id="message"
-//                                 placeholder="Enter your message"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-
-//                         <div className="flex justify-end pt-4">
-//                             <button
-//                                 className="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
-//                                 onClick={toggleCommentForm}
-//                             >
-//                                 Cancel
-//                             </button>
-//                             <button
-//                                 className="px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400"
-//                                 type="submit"
-//                             >
-//                                 Submit
-//                             </button>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Comment;
-
-// import React, { useState } from "react";
-// import { useAppContext } from "../context/AppContext";
-// import Header from "./Header.jsx";
-
-// const Comment = ({ toggleCommentForm }) => {
-//     const [formData, setFormData] = useState({
-//         question1: "",
-//         question2: "",
-//         question3: "",
-//     });
-//     const { user } = useAppContext();
-
-//     const handleChange = (e) => {
-//         setFormData({ ...formData, [e.target.name]: e.target.value });
-//     };
-
-//     const handleSubmit = (e) => {
-//         e.preventDefault();
-//         // Handle form submission here
-//         console.log(formData);
-//         toggleCommentForm();
-//     };
-
-//     return (
-//         <>
-//             <Header />
-//             <div className="flex justify-center items-center h-screen mt-[-80px]">
-//                 <div className="w-96 bg-white border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg">
-//                     <div className="p-4 border-b">
-//                         <h2 className="text-xl font-medium">Add Comment</h2>
-//                     </div>
-//                     <form className="p-4" onSubmit={handleSubmit}>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="block text-gray-700 font-bold mb-2 text-xl font mb-4"
-//                                 htmlFor="question1"
-//                             >
-//                                 How Can we Help You?
-//                             </label>
-//                             <input
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 type="text"
-//                                 name="question1"
-//                                 id="question1"
-//                                 placeholder="Your Comments"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="block text-gray-700 font-bold mb-2 text-xl font mb-4"
-//                                 htmlFor="question2"
-//                             >
-//                                 How Can we Improve
-//                             </label>
-//                             <input
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 type="text"
-//                                 name="question2"
-//                                 id="question2"
-//                                 placeholder="Your Comments"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-//                         <div className="mb-4">
-//                             <label
-//                                 className="block text-gray-700 font-bold mb-2 text-xl font mb-4"
-//                                 htmlFor="message"
-//                             >
-//                                 Details
-//                             </label>
-//                             <textarea
-//                                 className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-//                                 name="message"
-//                                 id="message"
-//                                 placeholder="Enter your message"
-//                                 onChange={handleChange}
-//                             />
-//                         </div>
-
-//                         <div className="flex justify-end pt-4">
-//                             <button
-//                                 className="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
-//                                 onClick={toggleCommentForm}
-//                             >
-//                                 Cancel
-//                             </button>
-//                             <button
-//                                 className="px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400"
-//                                 type="submit"
-//                             >
-//                                 Submit
-//                             </button>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
-// export default Comment;
-
 import React, { useState } from "react";
 import { useAppContext } from "../context/AppContext";
 import Header from "./Header.jsx";
 import SpinnerIcon from "./icons/SpinnerIcon";
+import { useThemeContext } from "../context/ThemeContext";
 
 const Comment = ({ toggleCommentForm }) => {
+    const { isDarkMode } = useThemeContext();
     const [formData, setFormData] = useState({
         question1: "",
         question2: "",
@@ -238,10 +34,21 @@ const Comment = ({ toggleCommentForm }) => {
     return (
         <>
             <Header />
+            {/* {`${
+        isDarkMode ? "dark" : "light"
+      } flex flex-col items-center justify-center w-full h-full gap-4 py-3 rounded-2xl shadow-md text-center`} */}
+            {/* "w-96 bg-white border border-gray-200 rounded-md p-4 hover:border-gray-500 hover:shadow-lg" */}
             <div className="flex justify-center items-center h-screen mt-[-80px]">
-                <div className="w-96 border border-gray-200 rounded-md p-4 hover:border-blue-500 hover:shadow-lg">
-                    <div className="p-4 border-b">
-                        <h2 className="text-xl font-medium">Add Comment</h2>
+                <div
+                    className={`${
+                        isDarkMode ? "dark" : "light"
+                    } flex flex-col gap-4 items-center justify-center bg-neutral-500 bg-neutral-500 rounded-2xl p-4 shadow-md text-left`}
+                >
+                    {/* text-center text-xl font-semibold py-3 */}
+                    <div className="p-3 border-b">
+                        <h2 className="text-center text-xl font-semibold py-3">
+                            Add Comment
+                        </h2>
                     </div>
                     <form className="p-4" onSubmit={onSubmitHandler}>
                         <div className="mb-4">
@@ -294,11 +101,12 @@ const Comment = ({ toggleCommentForm }) => {
 
                         <div className="flex justify-end pt-4">
                             <button
-                                className="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2"
+                                className="flex cursor-pointer bg-indigo-700 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600"
                                 onClick={toggleCommentForm}
                             >
                                 Cancel
                             </button>
+
                             <button
                                 disabled={loading}
                                 onClick={onSubmitHandler}
