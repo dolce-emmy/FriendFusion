1ba35538e8bff035311cbc23df1860c2bac25d4c
Eman Yassin <eman.yassin83@gmail.com>
2023-06-01 18:17:29 +0200

    changed chatGpt avatar and increased the seconds in the set interval on youtube

--- client/src/components/ChatGpt.jsx
+++ client/src/components/ChatGpt.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
+
 
 import {
   MainContainer,
