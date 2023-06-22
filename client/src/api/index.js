e8719806e6bee832472a15eaa469365f14d1801f
Eman Yassin <eman.yassin83@gmail.com>
2023-06-22 13:09:51 +0200

    added a new deployment

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: process.env.NODE_ENV === "development" ? "/api" : "",
   headers: {
     // Authorization: `Bearer ${localStorage.getItem("token")}`,
     token: localStorage.getItem("token"),
